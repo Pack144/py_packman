0de3d9e0f0bc44e7085f6ec1923956779fd97717
John Lawrence <thirdjal@users.noreply.github.com>
2018-09-03 23:49:42 -0700

    Migrate to django-ckeditor for rich-text

--- pages/models.py
+++ pages/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from tinymce.models import HTMLField
+from ckeditor.fields import RichTextField
 
 
 class Category(models.Model):
@@ -14,7 +14,7 @@ class Category(models.Model):
 
 class Page(models.Model):
     title = models.CharField(max_length=255)
-    body = HTMLField()
+    body = RichTextField()
     post_date = models.DateField()
     attachment = models.FileField(upload_to='pages/', null=True, blank=True)
     category = models.ManyToManyField(Category)
