f98c3cadf9acba046064e17d8c90c0c0d42d4c59
John Lawrence <thirdjal@users.noreply.github.com>
2019-05-04 15:30:49 -0700

    Add birthday field to Member model

--- membership/models.py
+++ membership/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class WebsiteLogin(AbstractUser):
@@ -50,6 +51,8 @@ class Member(models.Model):
     last_name = models.CharField(max_length=150)
     email = models.EmailField(blank=True, null=True)
 
+    date_of_birth = models.DateField(blank=True, null=True)
+
     CUB = 'S'
     GUARDIAN = 'G'
     CONTRIBUTOR = 'C'
@@ -72,6 +75,13 @@ class Member(models.Model):
 
     def __str__(self):
         return self.get_short_name()
+
+    def age(self):
+        if not self.date_of_birth:
+            return None
+        today = timezone.now()
+        return today.year - self.date_of_birth.year - (
+                    (today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day))
 
     def get_full_name(self):
         return "{} {}".format(self.get_short_name, self.last_name).strip()
