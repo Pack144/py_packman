a5cd7c8cbda2761e08805612abac20c064995c06
John Lawrence <thirdjal@users.noreply.github.com>
2019-05-30 21:35:53 -0500

    Update rank display

--- assignments/models.py
+++ assignments/models.py
@@ -4,30 +4,6 @@ from django.urls import reverse
 from pages.models import DynamicPage
 
 
-class Rank(models.Model):
-    BOBCAT = 1
-    TIGER = 2
-    WOLF = 3
-    BEAR = 4
-    JR_WEBE = 5
-    SR_WEBE = 6
-    ARROW = 7
-    RANK_CHOICES = (
-        (BOBCAT, "Bobcat"),
-        (TIGER, "Tiger"),
-        (WOLF, "Wolf"),
-        (BEAR, "Bear"),
-        (JR_WEBE, "Jr. Webelos"),
-        (SR_WEBE, "Sr. Webelos"),
-        (ARROW, "Arrow of Light"),
-    )
-
-    id = models.PositiveSmallIntegerField(choices=RANK_CHOICES, primary_key=True)
-
-    def __str__(self):
-        return self.get_id_display()
-
-
 class Committee(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
@@ -70,13 +46,13 @@ class Den(models.Model):
         (ARROW, "Arrow of Light"),
     )
 
-    rank = models.PositiveSmallIntegerField(choices=RANK_CHOICES, default=2)
+    rank = models.PositiveSmallIntegerField(choices=RANK_CHOICES, null=True, blank=True)
 
     class Meta:
         ordering = ['number']
 
     def __str__(self):
-        return 'Den {}: {}'.format(self.number, self.get_rank_display())
+        return 'Den {}'.format(self.number)
 
     def get_absolute_url(self):
         return reverse('den-detail', args=[str(self.number)])
