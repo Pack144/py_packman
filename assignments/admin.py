875bb5522c456fa1ac6d4ba49ecd6746af660e9c
John Lawrence <thirdjal@users.noreply.github.com>
2019-06-12 15:53:35 -0700

    Add detail to Den admin pages

--- assignments/admin.py
+++ assignments/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Committee, Den
 
 
-class PageInline(admin.StackedInline):
+class CommitteePageInline(admin.StackedInline):
     model = Committee.page.through
     extra = 0
     verbose_name = 'web page'
@@ -13,11 +13,21 @@ class PageInline(admin.StackedInline):
 class CommitteeAdmin(admin.ModelAdmin):
     prepopulated_fields = {'permalink': ('name', )}
     exclude = ['page']
-    inlines = [PageInline]
+    inlines = [CommitteePageInline]
+
+
+class DenPageInline(admin.StackedInline):
+    model = Den.page.through
+    extra = 0
+    verbose_name = 'web page'
+    verbose_name_plural = 'web pages'
 
 
 class DenAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('number', 'rank')
+    list_filter = ('rank', )
+    exclude = ['page']
+    inlines = [DenPageInline]
 
 
 admin.site.register(Committee, CommitteeAdmin)
